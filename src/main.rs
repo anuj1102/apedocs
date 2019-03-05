ab21c5ce9f0ada4e4d32e2d5a7ee324c95f4c50d
Anuj Patel <anuj@Anujs-MacBook-Pro.local>
2019-03-05 07:30:51 -0800

    main.rs: Blocking example for getting events
    Got an example of receiving events using the blocking recv
    mpsc channel call.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use neovim_lib::{Neovim, NeovimApi, Session};
 
-fn main() {
+#[allow(dead_code)]
+fn quit_example() {
     let mut session = Session::new_tcp("127.0.0.1:6666").unwrap();
     // let mut session = Session::new_child().unwrap();
     session.start_event_loop();
@@ -9,4 +10,43 @@ fn main() {
         Ok(()) => println!("quit success!"),
         Err(msg) => println!("error! {:?}", msg)
     }
+}
+
+fn notify_blocking_example() {
+    let mut session = Session::new_tcp("127.0.0.1:6666").unwrap();
+    let receiver = session.start_event_loop_channel();
+    let mut nvim = Neovim::new(session);
+    //https://github.com/boxofrox/neovim-scorched-earth/blob/master/src/main.rs
+    nvim.command("echom \"rust client connected to neovim\"").unwrap();
+    nvim.subscribe("cursor-moved-i").expect("error: cannot subscribe to event: change-cursor-i");
+    nvim.subscribe("insert-enter").expect("error: cannot subscribe to event: insert-enter");
+    nvim.subscribe("insert-leave").expect("error: cannot subscribe to event: insert-leave");
+    nvim.subscribe("quit").expect("error: cannot subscribe to event: quit");
+
+
+    let buffer = nvim.get_current_buf().unwrap();
+    buffer
+        .set_lines(
+            &mut nvim,
+            0,
+            1,
+            true,
+            vec![String::from("foo"), String::from("hey rust")],
+        )
+        .unwrap();
+
+    buffer.attach(&mut nvim, true, Vec::new()).unwrap();
+
+    println!("Starting blocking event loop");
+    loop {
+        match receiver.recv() {
+            Ok(s) => println!("We did it! Got {:?}", s),
+            Err(e) => println!("Got an error!: {:?}", e),
+        }
+    }
+    println!("broke from recv");
+}
+
+fn main() {
+    notify_blocking_example()
 }
