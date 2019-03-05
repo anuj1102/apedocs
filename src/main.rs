9b92a46894b8845f78b463f2dbc0bb36f09f057e
Anuj Patel <anuj@Anujs-MacBook-Pro.local>
2019-03-05 06:32:55 -0800

    main.rs: Quit example
    This is a small example that sends the quit command to neovim. To run start
    nvim with:
      nvim --listen 127.0.0.1:6666
    and then run:
      cargo run
    
    I'm getting a panic at the end:
    error! GenericError("Error read response: I/O error while
    reading marker byte: failed to fill whole buffer")

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use neovim_lib::{Neovim, NeovimApi, Session};
+
 fn main() {
-    println!("Hello, world!");
+    let mut session = Session::new_tcp("127.0.0.1:6666").unwrap();
+    // let mut session = Session::new_child().unwrap();
+    session.start_event_loop();
+    let mut nvim = Neovim::new(session);
+    match nvim.quit_no_save() {
+        Ok(()) => println!("quit success!"),
+        Err(msg) => println!("error! {:?}", msg)
+    }
 }
