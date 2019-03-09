6d5ab889b884174fdb9820a5b1532963d5ed447b
Anuj Patel <anuj@Anujs-MacBook-Pro.local>
2019-03-09 09:58:23 -0800

    main.rs: Refactor parsing
    This commit moves all of the parsing logic into it's own function and returns
    a struct to represent the nvim buf line events. For now we are copying the
    data into the vector for simplicity, but I think we can do better in the future.
    
    It also cleans up a lot of the setup logic and changes many of the unwraps
    to handle errors properly.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use neovim_lib::{Neovim, NeovimApi, Session};
 use neovim_lib::{Handler, Value};
+use neovim_lib::neovim_api::Buffer;
 use std::sync::mpsc;
 
 #[allow(dead_code)]
@@ -13,7 +14,7 @@ fn quit_example() {
     }
 }
 
-fn attach_to_nvim(nvim: &mut Neovim) -> (neovim_lib::neovim_api::Buffer) {
+fn attach_to_nvim(nvim: &mut Neovim) -> Buffer {
     //https://github.com/boxofrox/neovim-scorched-earth/blob/master/src/main.rs
     nvim.command("echom \"rust client connected to neovim\"").unwrap();
     let buffer = nvim.get_current_buf().unwrap();
@@ -31,47 +32,74 @@ fn attach_to_nvim(nvim: &mut Neovim) -> (neovim_lib::neovim_api::Buffer) {
     return buffer;
 }
 
+fn send_events_to_buffer(nvim: &mut Neovim, buffer: &Buffer) {
+    buffer
+        .set_lines(
+            nvim,
+            0,
+            1,
+            true,
+            vec![String::from("foo"), String::from("hey rust")],
+            )
+        .unwrap();
+}
+
+#[derive(Debug)]
+struct BufLineEvent {
+    tick: u64,
+    start: u64,
+    end: u64,
+    data: Vec<String>,
+}
+
+fn parse_nvim_buf_lines_event(v: &Vec<Value>) -> Result<BufLineEvent, &'static str> {
+    match v.as_slice() {
+        [_, ticker, line_start, line_end, lines, _] => {
+            match (ticker.as_u64(),
+                   line_start.as_u64(),
+                   line_end.as_u64(),
+                   lines.as_array()) {
+                (Some(t),Some(s), Some(e), Some(data)) => {
+                   let data_parsed : Vec<String> =
+                        data.iter().map(|s| String::from(s.as_str().unwrap())).collect();
+                    Ok(BufLineEvent {
+                        tick: t,
+                        start: s,
+                        end: e,
+                        data: data_parsed.clone(),
+                    })
+                }
+                _ => return Err("ticker did not parse"),
+            }
+        },
+        _ => return Err("bar"),
+    }
+}
+
 fn notify_blocking_example() {
     let mut session = Session::new_tcp("127.0.0.1:6666").unwrap();
-    let writer = session.start_event_loop_channel();
+    let _writer = session.start_event_loop_channel();
     let mut nvim = Neovim::new(session);
-    attach_to_nvim(&mut nvim);
+    let read_user_buf = attach_to_nvim(&mut nvim);
 
     let mut session2 = Session::new_tcp("127.0.0.1:6667").unwrap();
-    let writer = session2.start_event_loop_channel();
+    let write_user = session2.start_event_loop_channel();
     let mut nvim2 = Neovim::new(session2);
-    attach_to_nvim(&mut nvim);
-    let read_buf = attach_to_nvim(&mut nvim2);
+    attach_to_nvim(&mut nvim2);
 
     println!("Starting blocking event loop");
     loop {
-        match writer.recv() {
+        match write_user.recv() {
             Ok((event_type, v)) => {
-                match v.as_slice() {
-                    [_, tick, line_start, line_end, lines, _] => {
-                        println!("tick: {:?}", tick.as_u64().unwrap());
-                        println!("line_start: {:?}", line_start.as_u64());
-                        println!("line_end: {:?}", line_end.as_u64());
-                        let data = lines.as_array().unwrap();
-                        let data_parsed : Vec<&str>= data.iter().map(|s| s.as_str().unwrap()).collect();
-                        println!("data_parsed: {:?}", data_parsed);
-                    },
-                    _ => println!("hil"),
+                match parse_nvim_buf_lines_event(&v) {
+                    Ok(b) => { println!("Got event: {:?}", b); }
+                    Err(s) => { println!("Error: {:?}", s); }
                 }
-                read_buf
-                    .set_lines(
-                        &mut nvim,
-                        0,
-                        1,
-                        true,
-                        vec![String::from("foo"), String::from("hey rust")],
-                        )
-                    .unwrap();
+                send_events_to_buffer(&mut nvim, &read_user_buf);
             }
             Err(e) => println!("Got an error!: {:?}", e),
         }
     }
-    println!("broke from recv");
 }
 
 fn main() {
