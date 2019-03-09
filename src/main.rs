43b3ab04ae2ce5c335b1552bae0a305b2530fc93
Anuj Patel <anuj@Anujs-MacBook-Pro.local>
2019-03-08 21:28:53 -0800

    main.rs: Got basic example of write and read
    Got a basic example of reading from one nvim instance and writing to the other
    one. To test, run the following in seperate terminals:
    
    nvim --listen 127.0.0.1:6666
    nvim --listen 127.0.0.1:6667
    cargo run
    
    Type in the instance on port 6667 and you should see the other one fill up
    with "hey rust" messages. I'm not handling errors right now, so it breaks
    whenever nvim has an issue. In the future we should handle the errors in a
    log file and then reattach to the buffer.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use neovim_lib::{Neovim, NeovimApi, Session};
+use neovim_lib::{Handler, Value};
+use std::sync::mpsc;
 
 #[allow(dead_code)]
 fn quit_example() {
     let mut session = Session::new_tcp("127.0.0.1:6666").unwrap();
-    // let mut session = Session::new_child().unwrap();
     session.start_event_loop();
     let mut nvim = Neovim::new(session);
     match nvim.quit_no_save() {
@@ -12,10 +13,7 @@ fn quit_example() {
     }
 }
 
-fn notify_blocking_example() {
-    let mut session = Session::new_tcp("127.0.0.1:6666").unwrap();
-    let receiver = session.start_event_loop_channel();
-    let mut nvim = Neovim::new(session);
+fn attach_to_nvim(nvim: &mut Neovim) -> (neovim_lib::neovim_api::Buffer) {
     //https://github.com/boxofrox/neovim-scorched-earth/blob/master/src/main.rs
     nvim.command("echom \"rust client connected to neovim\"").unwrap();
     nvim.subscribe("cursor-moved-i").expect("error: cannot subscribe to event: change-cursor-i");
@@ -27,7 +25,7 @@ fn notify_blocking_example() {
     let buffer = nvim.get_current_buf().unwrap();
     buffer
         .set_lines(
-            &mut nvim,
+            nvim,
             0,
             1,
             true,
@@ -35,12 +33,37 @@ fn notify_blocking_example() {
         )
         .unwrap();
 
-    buffer.attach(&mut nvim, true, Vec::new()).unwrap();
+    buffer.attach(nvim, true, Vec::new()).unwrap();
+    return buffer;
+}
+
+fn notify_blocking_example() {
+    let mut session = Session::new_tcp("127.0.0.1:6666").unwrap();
+    let writer = session.start_event_loop_channel();
+    let mut nvim = Neovim::new(session);
+    attach_to_nvim(&mut nvim);
+
+    let mut session2 = Session::new_tcp("127.0.0.1:6667").unwrap();
+    let writer = session2.start_event_loop_channel();
+    let mut nvim2 = Neovim::new(session2);
+    attach_to_nvim(&mut nvim);
+    let read_buf = attach_to_nvim(&mut nvim2);
 
     println!("Starting blocking event loop");
     loop {
-        match receiver.recv() {
-            Ok(s) => println!("We did it! Got {:?}", s),
+        match writer.recv() {
+            Ok(s) => {
+                println!("We did it! Got {:?} Setting linesin read buf", s);
+                read_buf
+                    .set_lines(
+                        &mut nvim,
+                        0,
+                        1,
+                        true,
+                        vec![String::from("foo"), String::from("hey rust")],
+                    )
+                    .unwrap();
+            }
             Err(e) => println!("Got an error!: {:?}", e),
         }
     }
