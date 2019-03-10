f5ab98a7c78fd4c383eaa1ebf58ef9343ced67a6
Anuj Patel <anuj@Anujs-MacBook-Pro.local>
2019-03-09 17:47:33 -0800

    main.rs: Initial POC for sharing buffers
    This commit gets the initial POC of writing in one nvim instance and reading
    it out in another nvim instance working. It looks like there is some visible
    problems that force us to redraw the entire line for each insert in a line
    longer than 3 characters. After some more investigation I found that the I
    was getting into "insert completion" mode which was sending two events for each
    character in a line every time I appended a character.
    
    For example when entering the chars "fed", when I pressed "d" I would get
    Got event: BufLineEvent { tick: 64, start: 2, end: 3, data: ["fed"], more: false }
    Got event: BufLineEvent { tick: 65, start: 2, end: 3, data: ["fe"], more: false }
    Got event: BufLineEvent { tick: 66, start: 2, end: 3, data: ["f"], more: false }
    Got event: BufLineEvent { tick: 67, start: 2, end: 3, data: [""], more: false }
    Got event: BufLineEvent { tick: 68, start: 2, end: 3, data: ["f"], more: false }
    Got event: BufLineEvent { tick: 69, start: 2, end: 3, data: ["fe"], more: false }
    Got event: BufLineEvent { tick: 70, start: 2, end: 3, data: ["fed"], more: false }
    
    I haven't narrowed down what plugin is responsible for this. For now I'm
    changing my test setup to ignore my init.vim. To test run the following
    in seperate terminal sessions:
    
    nvim -u foo --listen 127.0.0.1:6666 => See the text read out here
    nvim -u foo --listen 127.0.0.1:6667 => Write into this instance
    cargo run

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use neovim_lib::{Neovim, NeovimApi, Session};
 use neovim_lib::{Handler, Value};
+use neovim_lib::neovim;
 use neovim_lib::neovim_api::Buffer;
 use std::sync::mpsc;
 
@@ -16,7 +17,9 @@ fn quit_example() {
 
 fn attach_to_nvim(nvim: &mut Neovim) -> Buffer {
     //https://github.com/boxofrox/neovim-scorched-earth/blob/master/src/main.rs
-    nvim.command("echom \"rust client connected to neovim\"").unwrap();
+    nvim.command("echom \"rust client connected to neovim\"")
+        .expect("Unable to connect to neovim instance");
+
     let buffer = nvim.get_current_buf().unwrap();
     buffer
         .set_lines(
@@ -32,34 +35,39 @@ fn attach_to_nvim(nvim: &mut Neovim) -> Buffer {
     return buffer;
 }
 
-fn send_events_to_buffer(nvim: &mut Neovim, buffer: &Buffer) {
-    buffer
-        .set_lines(
-            nvim,
-            0,
-            1,
-            true,
-            vec![String::from("foo"), String::from("hey rust")],
-            )
-        .unwrap();
-}
-
 #[derive(Debug)]
 struct BufLineEvent {
     tick: u64,
-    start: u64,
-    end: u64,
+    start: i64,
+    end: i64,
     data: Vec<String>,
+    more: bool,
+}
+
+fn send_events_to_buffer(nvim: &mut Neovim, buffer: &Buffer, buf_event: BufLineEvent) {
+    let result = buffer
+                    .set_lines(
+                        nvim,
+                        buf_event.start,
+                        buf_event.end,
+                        true,
+                        buf_event.data,
+                    );
+    match result {
+        Ok(()) => return,
+        Err(s) => println!("Error with send events: {:?}", s),
+    }
 }
 
 fn parse_nvim_buf_lines_event(v: &Vec<Value>) -> Result<BufLineEvent, &'static str> {
     match v.as_slice() {
-        [_, ticker, line_start, line_end, lines, _] => {
+        [_, ticker, line_start, line_end, lines, more] => {
             match (ticker.as_u64(),
-                   line_start.as_u64(),
-                   line_end.as_u64(),
-                   lines.as_array()) {
-                (Some(t),Some(s), Some(e), Some(data)) => {
+                   line_start.as_i64(),
+                   line_end.as_i64(),
+                   lines.as_array(),
+                   more.as_bool()) {
+                (Some(t),Some(s), Some(e), Some(data), Some(more_data)) => {
                    let data_parsed : Vec<String> =
                         data.iter().map(|s| String::from(s.as_str().unwrap())).collect();
                     Ok(BufLineEvent {
@@ -67,6 +75,7 @@ fn parse_nvim_buf_lines_event(v: &Vec<Value>) -> Result<BufLineEvent, &'static s
                         start: s,
                         end: e,
                         data: data_parsed.clone(),
+                        more: more_data,
                     })
                 }
                 _ => return Err("ticker did not parse"),
@@ -92,10 +101,12 @@ fn notify_blocking_example() {
         match write_user.recv() {
             Ok((event_type, v)) => {
                 match parse_nvim_buf_lines_event(&v) {
-                    Ok(b) => { println!("Got event: {:?}", b); }
+                    Ok(buf_event) => {
+                        println!("Got event: {:?}", buf_event);
+                        send_events_to_buffer(&mut nvim, &read_user_buf, buf_event);
+                    }
                     Err(s) => { println!("Error: {:?}", s); }
                 }
-                send_events_to_buffer(&mut nvim, &read_user_buf);
             }
             Err(e) => println!("Got an error!: {:?}", e),
         }
